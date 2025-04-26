6f96292426d3ceabe2a97f26d61d482dd8122451
Abdou2111 <abdelghafour.rahmouni@gmail.com>
2025-04-25 20:02:37 -0400

    Bornes_recharges and foresterie done

--- bornes_recharges/MinCostRechargeCalculator.cpp
+++ bornes_recharges/MinCostRechargeCalculator.cpp
@@ -16,6 +16,29 @@ MinCostRechargeCalculator::MinCostRechargeCalculator()
 }
 
 int MinCostRechargeCalculator::CalculateMinCostRecharge(const vector<int>& RechargeCost) {
-   // Fonction à compléter / function to complete
-   return 0;
+   int nombre_de_bornes = RechargeCost.size();
+   // Les conditions d'initialisation de l'algorithme
+
+   // Si le nombre de bornes est inférieur à 3, le coût est 0
+   if (nombre_de_bornes < 3) return 0;
+
+   // Si le nombre de bornes est égal à 3, le coût est le minimum des trois bornes
+   else if (nombre_de_bornes == 3) {
+      return min(RechargeCost[0], min(RechargeCost[1], RechargeCost[2]));
+   }
+
+   // Si le nombre de bornes est supérieur à 3, on utilise la formule de récurrence
+   else {
+      // initialiser le tableau cout avec les trois premiers coûts de recharge
+      vector<int> cout(nombre_de_bornes, 0);
+      cout[0] = RechargeCost[0];
+      cout[1] = RechargeCost[1];
+      cout[2] = RechargeCost[2];
+
+      // On remplir la lsite des coûts en utilisant la formule de récurrence
+      for (int i = 3; i < nombre_de_bornes; ++i)
+         cout[i] = RechargeCost[i] + min(cout[i - 1], min(cout[i - 2], cout[i - 3]));
+         return min(cout[nombre_de_bornes - 1],
+            min(cout[nombre_de_bornes - 2], cout[nombre_de_bornes - 3]));
+   }
 }
--- foresterie/foresterie.py
+++ foresterie/foresterie.py
@@ -7,10 +7,39 @@ import sys
 # Space for auxilary functions :
 
 
-
 # Fonction à compléter / function to complete:
-def solve(cost, forest) :
-    return 
+def solve(cost, forest):
+    # Conditions d'initialisation:
+
+    # Si la forêt est vide, il n'y a pas d'arbre à couper
+    if len(forest) == 0:
+        return 0
+
+        # Si le coût d'exploitation est négatif, il n'y a pas de profit
+    if cost < 0:
+        return -1
+
+    # Si il n'y a qu'un arbre dans la forêt,
+    # Il faut comparer le profit de le couper ou de ne pas le couper.
+    if len(forest) == 1:
+        return max(0, forest[0] - cost)
+
+    # Si il y a deux arbres dans la forêt, on regarde
+    # le profit de couper le premier arbre ou le deuxième arbre.
+    # et le profit de ne pas couper d'arbre.
+    if len(forest) == 2:
+        return max(0, forest[0] - cost, forest[1] - cost)
+
+    # Si il y a plus de deux arbres dans la forêt,
+    # il faut utiliser l'algorithme de programmation dynamique.
+    profit = [0] * len(forest)
+    profit[0] = forest[0] - cost
+    profit[1] = max(forest[0] - cost, forest[1] - cost)
+    # On remplit la liste de profit en utilisant la relation de récurrence.
+    for i in range(2, len(forest)):
+        profit[i] = max(profit[i - 1], profit[i - 2] + forest[i] - cost)
+    # On retourne le profit maximal.
+    return profit[-1]
 
 
 # Ne pas modifier le code ci-dessous :
